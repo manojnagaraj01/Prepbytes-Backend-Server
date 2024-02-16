bc18ec6284488162293d10837a2636a58101787e
manojnagaraj01 <n.manoj9535@gmail.com>
2024-02-16 12:07:21 +0530

    cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyparser.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: ['*', 'https://m.stripe.network', 'https://js.stripe.com', "http://localhost:5173","https://stripe-payments-app.herokuapp.com","https://stripe.com/" ]
+    origin: ['*', 'https://m.stripe.network', 'https://js.stripe.com', "http://localhost:5173","https://stripe-payments-app.herokuapp.com","https://stripe.com/" , "https://prepbytes-backend-server.onrender.com"]
   }));
 //database
 const {dbconnect} = require("./config/dbconnect")
