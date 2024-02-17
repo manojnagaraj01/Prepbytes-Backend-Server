2a73ced12972c30afb7576773f259e4717ed5721
manojnagaraj01 <n.manoj9535@gmail.com>
2024-02-17 09:17:08 +0530

    origin

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyparser.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: ['*', 'https://m.stripe.network', 'https://js.stripe.com', "http://localhost:5173","https://stripe-payments-app.herokuapp.com","https://stripe.com/" , "https://prepbytes-backend-server.onrender.com", "https://prepbytes-clone-three.vercel.app", "https://prepbytes-backend-server.onrender.com/user/create-checkout-session"]
+    origin: ['*', 'https://m.stripe.network', 'https://js.stripe.com', "http://localhost:5173","https://stripe-payments-app.herokuapp.com","https://stripe.com/" , "https://prepbytes-backend-server.onrender.com", "https://prepbytes-clone-three.vercel.app", "https://prepbytes-backend-server.onrender.com/user/create-checkout-session", "https://prepbytes-backend-server.onrender.com/user/create-order", "https://prepbytes-backend-server.onrender.com/user/create-order",]
   }));
 //database
 const {dbconnect} = require("./config/dbconnect")
